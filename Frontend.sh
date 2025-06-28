c6bb037481f9bc95726fa4ab43358ad1de926a6a
labhinav <lakkarajabhinav@gmail.com>
2025-06-28 09:16:11 -0700

    Frontend-roboshop

--- Frontend.sh
+++ Frontend.sh
@@ -33,14 +33,14 @@ VALIDATE(){
     fi
 }
 
-dnf module disable nginx -y &>>LOG_FILE
+dnf module disable nginx -y &>>$LOG_FILE
 VALIDATE $? "Disabling Default Nginx"
 
-dnf module enable nginx:1.24 -y &>>LOG_FILE
-VALIDATE $? "Enabling Nginx"
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+VALIADTE $? "Enabling Nginx:1.24"
 
-dnf install nginx -y &>>LOG_FILE
-VALIDATE $? "Installing Nginx"
+dnf install nginx -y &>>$LOG_FILE
+VALIADTE $? "Installing Nginx"
 
 systemctl enable nginx &>>$LOG_FILE
 systemctl start nginx
@@ -50,17 +50,17 @@ rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 VALIADTE $? "Removing default content"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading frontend"
+VALIADTE $? "Downloading frontend"
 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>$LOG_FILE
-VALIDATE $? "unzipping frontend"
+VALIADTE $? "unzipping frontend"
 
 rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
-VALIDATE $? "Remove default nginx conf"
+VALIADTE $? "Remove default nginx conf"
 
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
-VALIDATE $? "Copying nginx.conf"
+VALIADTE $? "Copying nginx.conf"
 
-systemctl restart nginx 
-VALIDATE $? "Restarting nginx"
+systemctl restart nginx
+VALIADTE $? "Restarting nginx"
