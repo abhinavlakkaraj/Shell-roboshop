80b66220f6983dd73832e3cb4d9f1a4c8e992b50
labhinav <lakkarajabhinav@gmail.com>
2025-08-13 16:08:49 -0700

    Catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -77,6 +77,11 @@ VALIDATE $? "Starting Catalogue"
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
-
-mongosh --host mongodb.daws84s.online </app/db/master-data.js &>>$LOG_FILE
-VALIDATE $? "Loading data into MongoDB"
+STATUS=$(mongosh --host mongodb.daws84s.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
+then
+    mongosh --host mongodb.daws84s.online </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Loading data into MongoDB"
+else
+    echo -e "Data is already loaded ... $Y SKIPPING $N"
+fi
