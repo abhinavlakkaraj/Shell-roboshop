ab1d82833cb961afb70bad417c4cfbb3e44bb0a3
labhinav <lakkarajabhinav@gmail.com>
2025-08-18 20:36:41 -0700

    shipping

--- shipping.sh
+++ shipping.sh
@@ -80,16 +80,10 @@ VALIDATE $? "Starting Shipping"
 dnf install mysql -y  &>>$LOG_FILE
 VALIDATE $? "Install MySQL"
 
-mysql -h mysql.daws84s.online -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
-if [ $? -ne 0 ]
-then
     mysql -h mysql.daws84s.online -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
     mysql -h mysql.daws84s.online -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
     mysql -h mysql.daws84s.online -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
     VALIDATE $? "Loading data into MySQL"
-else
-    echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
-fi
 
 systemctl restart shipping &>>$LOG_FILE
 VALIDATE $? "Restart shipping"
